140f1c5f926bbe39be632243e2a8808ae4498366
Vitalii Shubnik <vitalii.shubnik@gmail.com>
2022-08-12 16:22:27 +0300

    change auth to NgRx approach

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,58 +1,3 @@
-// import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-// import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
-// import { ComponentPortal, DomPortal, Portal } from '@angular/cdk/portal';
-// import { InputComponent } from '../input/input.component';
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.scss']
-// })
-// export class HomeComponent implements AfterViewInit, OnInit {
-//   @ViewChildren('dragItem') items: QueryList<ElementRef<HTMLElement>>
-
-
-
-
-
-
-//   // availablePortals:DomPortal<any>[] = [] ;
-//   domPortal: ComponentPortal<any>;
-//   selectedPortal: Portal<any>;
-
-//   componentPortals: ComponentPortal<any>[] = []
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//     this.componentPortals.push(new ComponentPortal(InputComponent));
-//     this.componentPortals.push(new ComponentPortal(InputComponent));
-//   }
-
-//   ngAfterViewInit() {
-//     this.componentPortals.push(new ComponentPortal(InputComponent));
-//     this.componentPortals.push(new ComponentPortal(InputComponent));
-//   }
-
-//   drop(ev: CdkDragDrop<ComponentPortal<any>[]>) {
-//     // if (this.componentPortals.filter(el => el.element == ev.item.element.nativeElement).length == 0) {
-//     //   console.log('new element')
-//     //   this.domElements.push(new ComponentPortal(ev.item.element))
-//     // }
-//     console.log(this.componentPortals)
-//   }
-
-//   dropBack(ev: CdkDragDrop<any[]>) {
-//     // if (this.domElements.find(el => el.element == ev.item.element.nativeElement) != undefined) {
-//     //   this.domElements = this.domElements.filter(el => el.element != ev.item.element.nativeElement)
-//     //   console.log("i am here")
-//     // }
-//   }
-// }
-
-
-
-
-
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CdkDragDrop, CdkDropList, copyArrayItem, moveItemInArray } from '@angular/cdk/drag-drop';
 import { AvailableItems } from '../../enums/availableItem';
@@ -72,7 +17,6 @@ export class HomeComponent {
   ) {
     this.styleService.currentElement.subscribe(el => {
       el.setAttribute('placeholder', Math.random().toString())
-      console.log(styleService.getElementCurrentStyleValues(el))
       this.current = styleService.getElementCurrentStyleValues(el)
     })
   }
@@ -106,9 +50,7 @@ export class HomeComponent {
     return false;
   }
 
-  remove(ev: MouseEvent, item) {
-    console.log(item)
-  }
+  
 }
 
 
--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { selectIsLoggedIn } from 'src/app/shared/selectors/auth.selector';
+import { Store } from '@ngrx/store';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +11,22 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private store: Store
   ) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.authService.isLoggedIn()) {
+    let s = false
+    this.store.select(selectIsLoggedIn).subscribe(el => s = el)
+    if (s) {
+      console.log('logged In')
+
       return true;
     }
     else {
+      console.log('not loggedIn')
       return this.router.parseUrl('/authenticate');
     }
   }
