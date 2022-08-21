e2d570396486bcbd1a7cf0332c3f557d6f4d713c
Vitalii Shubnik <vitalii.shubnik@gmail.com>
2022-08-22 01:53:40 +0300

    change drop implementation

--- src/app/core/modules/home/home.module.ts
+++ src/app/core/modules/home/home.module.ts
@@ -1,40 +1,31 @@
-import { NgModule } from '@angular/core';
-import { HomeComponent } from './home/home.component';
-import { InputComponent } from './form-elements/input/input.component';
-import { TextareaComponent } from './form-elements/textarea/textarea.component';
-import { SelectComponent } from './form-elements/select/select.component';
-import { CheckboxComponent } from './form-elements/checkbox/checkbox.component';
-import { ButtonComponent } from './form-elements/button/button.component';
-import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { PortalModule } from '@angular/cdk/portal';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { FirstSectionComponent } from './first-section/first-section.component';
-import { SecondSectionComponent } from './second-section/second-section.component';
-import { ThirdSectionComponent } from './third-section/third-section.component';
-import { MatSelectModule } from '@angular/material/select'
-import { MatInputModule } from '@angular/material/input';
+import { PortalModule } from '@angular/cdk/portal';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { NavbarComponent } from './navbar/navbar.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { PushModule } from '@ngrx/component';
+import { FirstSectionComponent } from './first-section/first-section.component';
+import { FormElementsModule } from './form-elements/form-elements.module';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SecondSectionComponent } from './second-section/second-section.component';
+import { ThirdSectionComponent } from './third-section/third-section.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    InputComponent,
-    TextareaComponent,
-    SelectComponent,
-    CheckboxComponent,
-    ButtonComponent,
     FirstSectionComponent,
     SecondSectionComponent,
     ThirdSectionComponent,
     NavbarComponent,
   ],
   imports: [
+    FormElementsModule,
     NoopAnimationsModule,
     BrowserAnimationsModule,
     PortalModule,
--- src/app/core/modules/home/third-section/third-section.component.ts
+++ src/app/core/modules/home/third-section/third-section.component.ts
@@ -1,19 +1,22 @@
-import { Component, Input} from '@angular/core';
-import { onDrop } from 'src/app/core/models/onDrop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, Input } from '@angular/core';
+import { Drop } from 'src/app/core/models/drop';
 
 @Component({
   selector: 'app-third-section',
   templateUrl: './third-section.component.html',
   styleUrls: ['./third-section.component.scss', '../home/home.component.scss']
 })
-export class ThirdSectionComponent extends onDrop {
-
-  constructor() {
-    super()
-  }
-
+export class ThirdSectionComponent implements Drop {
   @Input()
   available: any
+
+  constructor() {}
+
+  drop = (event: CdkDragDrop<any, any, any>) => {
+    if (event.previousContainer === event.container)
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
+  }
 
   noReturnPredicate() {
     return false;
