dd87bcc9b2e977b284e4e7501ee528c41e4d3d5a
Vitalii Shubnik <vitalii.shubnik@gmail.com>
2022-08-22 21:54:31 +0300

    add sections to create, update, delete form elements

--- src/app/core/modules/home/second-section/second-section.component.ts
+++ src/app/core/modules/home/second-section/second-section.component.ts
@@ -1,6 +1,7 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { CdkPortal } from '@angular/cdk/portal';
 import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { distinctUntilChanged, Observable, Subject, takeUntil } from 'rxjs';
 import { AvailableItems } from 'src/app/core/enums/availableItem';
@@ -10,6 +11,7 @@ import { PortalBridgeService } from 'src/app/core/services/portal-bridge.service
 import * as ElementActions from 'src/app/shared/actions/elementStyles.actions';
 import { selectElementStyles } from 'src/app/shared/selectors/elementStyles.selector';
 import { ElementStyles } from 'src/app/shared/statesModels/elementStyles.state';
+import { EditDialogComponent } from '../edit-dialog/edit-dialog.component';
 
 @Component({
   selector: 'app-second-section',
@@ -23,18 +25,39 @@ export class SecondSectionComponent implements Drop, OnInit, OnDestroy, AfterVie
   previousStyles: ElementStyles = null
   activeItem$ = this.formItemService.element$
   activeElementStyles$: Observable<ElementStyles> = this.store.select(selectElementStyles)
+  dragging: boolean = false
 
   destroy$: Subject<boolean> = new Subject<boolean>();
   @Input()
-  used: Array<any> = [];
+  used: any[] = [];
   removed: any[] = []
+  foredit: any[] = []
   constructor(
+    public dialog: MatDialog,
     private formItemService: FormItemService,
     private portalBridge: PortalBridgeService,
     private store: Store
   ) { }
+
   dropToRemove = (event: CdkDragDrop<any, any, any>) => {
     event.previousContainer.data.splice(event.previousIndex, 1)
+    this.formItemService.setActiveElement(null)
+    this.styles = {}
+    this.previousStyles = {}
+  }
+  dropToEdit = (event: CdkDragDrop<any, any, any>) => {
+    const dialogRef = this.dialog.open(EditDialogComponent, {
+      width: '250px',
+      data: event.previousContainer.data[event.previousIndex],
+    });
+
+    dialogRef.afterClosed().pipe(
+      takeUntil(this.destroy$),
+    ).subscribe(result => {
+      console.log('subscribed '+ result)
+      if (Array.isArray(result) || (typeof result === 'string' || result instanceof String))
+        event.previousContainer.data[event.previousIndex].data = result;
+    });
   }
 
   drop = (event: CdkDragDrop<any, any, any>) => {
